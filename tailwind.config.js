bbe0260572497f93e8d3893cd0ed3abec51bb733
Satyam Singh <satyam@satyam.local>
2023-09-12 10:36:15 +0530

    gita

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+    }
+  },
+
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
+  
+  plugins: [require("daisyui"),require('@tailwindcss/typography')],
+  
+}
+
