3b893247261591dd74b81ba44f9b4d892d8b5a74
Satyam Singh <satyam@satyam.local>
2023-09-15 15:30:41 +0530

    gita_about_01

--- src/pages/about_us.js
+++ src/pages/about_us.js
@@ -19,6 +19,13 @@ function About_us(){
     </div>
   </div>
 </div>
+<div>
+<div className="avatar">
+  <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+    <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+  </div>
+</div>
+</div>
 <Footer />
         </div>
     )
