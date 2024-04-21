e49368e17fba67f470a13932e3f3ada5969f0ddc
Satyam singh <satyamsingh2003a@gmail.com>
2024-04-21 20:05:35 +0530

    fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,12 @@
-import { Analytics } from "@vercel/analytics/react";
-import '@/styles/globals.css'; // Import global styles
-import { ThemeProvider } from 'next-themes';
-import { useEffect } from 'react'; // Import useEffect hook if needed
+import '@/styles/globals.css'
+import { ThemeProvider } from 'next-themes'
+
 
 export default function App({ Component, pageProps }) {
-  // Use useEffect to initialize analytics
-  useEffect(() => {
-    const analytics = Analytics();
-    analytics.page();
-  }, []);
-
   return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+  <ThemeProvider>
+
+  <Component {...pageProps} />
+</ThemeProvider>
+  )
 }
