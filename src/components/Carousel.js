f6ccf0716bd00d217e7ddd2a2e81a9b95b3869cd
Satyam singh <satyamsingh2003a@gmail.com>
2024-05-30 03:23:05 +0530

    added loading animation in 3D

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,32 +1,56 @@
 import React from "react";
-
-function Carousel (){
-    return (
-        <div>
-           <div className="carousel w-full">
-  <div id="slide1" className="carousel-item relative w-full">
-    <img src="https://www.bhagavad-gita.us/wp-content/uploads/2012/10/gita-02.jpg" className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide3" className="btn btn-circle">❮</a> 
-      <a href="#slide2" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-  <div id="slide2" className="carousel-item relative w-full">
-    <img src="http://www.bhagavad-gita.us/wp-content/uploads/2012/09/gita-119.jpg" className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide1" className="btn btn-circle">❮</a> 
-      <a href="#slide3" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-  <div id="slide3" className="carousel-item relative w-full">
-    <img src="https://www.bhagavad-gita.us/wp-content/uploads/2012/09/gita-109.jpg" className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide2" className="btn btn-circle">❮</a> 
-      <a href="#slide3" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-</div>
+import Image from "next/image";
+function Carousel() {
+  return (
+    <div>
+      <div className="carousel w-full">
+        <div id="slide1" className="carousel-item relative w-full">
+          <Image
+            src="https://www.bhagavad-gita.us/wp-content/uploads/2012/10/gita-02.jpg"
+            alt=""
+            className="w-full"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide2" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
         </div>
-    )
+        <div id="slide2" className="carousel-item relative w-full">
+          <Image
+            src="http://www.bhagavad-gita.us/wp-content/uploads/2012/09/gita-119.jpg"
+            alt=""
+            className="w-full"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide3" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide3" className="carousel-item relative w-full">
+          <Image
+            src="https://www.bhagavad-gita.us/wp-content/uploads/2012/09/gita-109.jpg"
+            alt=""
+            className="w-full"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide3" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Carousel;
