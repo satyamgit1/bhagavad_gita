dfa360a1f1ec18aaceacc1cc9f931c98b5f5eeef
Satyam singh <satyamsingh2003a@gmail.com>
2024-06-23 15:55:35 +0530

    added Authentication using Auth0

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -40,6 +40,22 @@ function Navbar() {
             <li>
               <Link href="/audiobook">Audio Book</Link>
             </li>
+            <li>{isAuthenticated && <p>{user.name}</p>}</li>
+          {isAuthenticated ? (
+            <li>
+              <button
+                onClick={() =>
+                  logout({ logoutParams: { returnTo: window.location.origin } })
+                }
+              >
+                Log Out
+              </button>
+            </li>
+          ) : (
+            <li>
+              <button onClick={() => loginWithRedirect()}>Log In</button>
+            </li>
+          )}
           </ul>
         </div>
         <Link href="/">
