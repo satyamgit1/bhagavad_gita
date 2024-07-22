06afe42aabad9f0f2efa144ba623522e88c08b0a
Satyam singh <satyamsingh2003a@gmail.com>
2024-07-22 22:31:50 +0530

    fix font of navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ function Navbar() {
   };
 
   return (
-    <div className="navbar bg-base-200">
+    <div className="navbar bg-base-200 text-lg font-bold ">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -33,7 +33,7 @@ function Navbar() {
           </label>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 "
           >
            <li>
             <Link href="/">Home Page</Link>
