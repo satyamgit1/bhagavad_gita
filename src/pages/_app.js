1edde8d0910a002f200f1064fd1120bc808c3def
Satyam singh <satyamsingh2003a@gmail.com>
2024-07-23 21:18:52 +0530

    implement SpeedInsights of vercel

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,7 @@ import '@/styles/verses.css';
 import { ThemeProvider } from 'next-themes';
 import { Auth0Provider } from '@auth0/auth0-react';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function App({ Component, pageProps }) {
   return (
@@ -17,6 +18,7 @@ export default function App({ Component, pageProps }) {
       <ThemeProvider>
         <Component {...pageProps} />
         <Analytics />
+        <SpeedInsights />
       </ThemeProvider>
     </Auth0Provider>
   );
