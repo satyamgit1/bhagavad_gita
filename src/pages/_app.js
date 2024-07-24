4037166547f4b1d93231a4f7dfab0cf1fc05acbe
Satyam singh <satyamsingh2003a@gmail.com>
2024-07-24 20:04:23 +0530

    added loading bar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,21 @@
-// pages/_app.js
 import '@/styles/globals.css';
 import '@/styles/verses.css';
+import '@/styles/nprogress.css'; // Import custom NProgress styles
 import { ThemeProvider } from 'next-themes';
 import { Auth0Provider } from '@auth0/auth0-react';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css'; // Import NProgress default styles
+
+// NProgress configuration
+NProgress.configure({ showSpinner: false });
+
+// Bind events to show/hide the progress bar
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function App({ Component, pageProps }) {
   return (
