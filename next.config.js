fea47d6228a76c5d488e5eb4238794a11d129f99
Satyam singh <satyamsingh2003a@gmail.com>
2024-07-26 12:11:21 +0530

    fix whatsapp logo

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env:{
-    MONGODB_URI: process.env.MONGODB_URI
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
   },
-}
+  images: {
+    domains: ['img.icons8.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
