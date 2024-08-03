8208938b64f4c87f8e715d9c9e16020cc176cd07
Satyam singh <satyamsingh2003a@gmail.com>
2024-08-03 15:12:07 +0530

    skeleton implemented

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,15 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
-    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
     container: {
       center: true,
-    }
+    },
   },
-
+  darkMode: 'class', // Enable dark mode support
   daisyui: {
     themes: [
       "light",
@@ -48,8 +46,8 @@ module.exports = {
       "winter",
     ],
   },
-  
-  plugins: [require("daisyui"),require('@tailwindcss/typography')],
-  
+  plugins: [
+    require("daisyui"),
+    require('@tailwindcss/typography')
+  ],
 }
-
