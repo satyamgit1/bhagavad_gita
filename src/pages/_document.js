38c8b39859682d39f42cfb013452f38d614cf0d8
Satyam singh <satyamsingh2003a@gmail.com>
2024-12-02 13:37:42 +0530

    <SpeedInsights />

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import { Analytics } from '@vercel/analytics/react';
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function Document() {
   return (
     <Html lang="en">
@@ -20,6 +20,7 @@ export default function Document() {
         <Main />
         <NextScript />
         <Analytics />
+        <SpeedInsights />
       </body>
     </Html>
   );
