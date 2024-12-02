df677021f34c133a00f47bf708dddac64ccb8404
Satyam singh <satyamsingh2003a@gmail.com>
2024-12-02 14:23:28 +0530

    contact_us bug

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -37,7 +37,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS_CONTACT,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
