4a4de36e719e48eac45193e4e474d9a26e4b969a
Satyam singh <satyamsingh2003a@gmail.com>
2024-12-02 14:38:34 +0530

    speed insignt

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 export default function Document() {
   return (
     <Html lang="en">
@@ -14,13 +15,15 @@ export default function Document() {
         <link rel="icon" href="/favicon.ico" />
         <meta name="theme-color" content="#000000" /> {/* Adjust your theme color */}
         
+        {/* Vercel Analytics and Speed Insights */}
+        <Analytics />
+        <SpeedInsights />
+        
         {/* Add any other custom head elements here */}
       </Head>
       <body>
         <Main />
         <NextScript />
-        <Analytics />
-        <SpeedInsights />
       </body>
     </Html>
   );
