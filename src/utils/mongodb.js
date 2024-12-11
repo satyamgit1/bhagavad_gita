ce1ca076da45642576a86e82652b95de1f777690
Satyam singh <satyamsingh2003a@gmail.com>
2024-12-11 09:31:09 +0530

    fix bug for sending the mail

--- src/utils/mongodb.js
+++ src/utils/mongodb.js
@@ -25,50 +25,18 @@
 
 
 // src/utils/mongodb.js
-// import { MongoClient } from 'mongodb';
-
-// const uri = process.env.MONGODB_URI;
-// const options = {};
-
-// let client;
-// let clientPromise;
-
-// if (!process.env.MONGODB_URI) {
-//   throw new Error('Please add your MongoDB URI to .env.local');
-// }
-
-// if (process.env.NODE_ENV === 'development') {
-//   if (!global._mongoClientPromise) {
-//     client = new MongoClient(uri, options);
-//     global._mongoClientPromise = client.connect();
-//   }
-//   clientPromise = global._mongoClientPromise;
-// } else {
-//   client = new MongoClient(uri, options);
-//   clientPromise = client.connect();
-// }
-
-// export default clientPromise;
-
-
 import { MongoClient } from 'mongodb';
 
-// Ensure MongoDB URI is in the environment variables
 const uri = process.env.MONGODB_URI;
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
+const options = {};
 
 let client;
 let clientPromise;
 
-// Check if the MongoDB URI is defined in the environment variables
-if (!uri) {
+if (!process.env.MONGODB_URI) {
   throw new Error('Please add your MongoDB URI to .env.local');
 }
 
-// If we're in development mode, use a global variable to reuse the connection (helps with hot reloading in Next.js)
 if (process.env.NODE_ENV === 'development') {
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options);
@@ -76,7 +44,6 @@ if (process.env.NODE_ENV === 'development') {
   }
   clientPromise = global._mongoClientPromise;
 } else {
-  // In production, we don't want to reuse connections, so we create a new client for each request
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
